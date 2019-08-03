dc1d6c395b810725b8203340173551b1826aec5f
Abhishek Kumar <ak6210@nyu.edu>
2019-08-03 02:08:31 -0400

    Modified sample response

--- api/controllers/resumeController.js
+++ api/controllers/resumeController.js
@@ -1,6 +1,6 @@
 module.exports = {
     getSample(req, res, next) {
         console.log("Request is :", req.body);
-        res.json({"Name": "Abhishek Kumar", "Role": "Software Engineer", "Company": "ABC"});
+        res.json('OK');
     }
 };
