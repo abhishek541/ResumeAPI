1606ce3451c53efaecc08ad77ce6680f4a21622c
Abhishek Kumar <ak6210@nyu.edu>
2019-08-03 21:30:30 -0400

    Modified data for puzzle

--- api/service/resumeService.js
+++ api/service/resumeService.js
@@ -2,6 +2,9 @@ const resumeData = require('./data');
 
 module.exports = {
     getTextResponse(query) {
+        if(query == "Puzzle") {
+            return resumeData[query].join('\n');
+        }
         return resumeData[query];
     }
 }
