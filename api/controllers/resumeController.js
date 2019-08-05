23b03d3d518eb88f6bbb8eaf83438a31b9f10c6c
Abhishek Kumar <ak6210@nyu.edu>
2019-08-04 23:06:55 -0400

    Modified log message for requests

--- api/controllers/resumeController.js
+++ api/controllers/resumeController.js
@@ -3,7 +3,7 @@ const resumeService = require('../service/resumeService');
 
 module.exports = {
     getSample(req, res, next) {
-        logger.log("info", `${req.method} ${req.url}    RequestType: ${req.query.q}    Question: ${req.query.d}`);
+        logger.log("info", `${req.method} ${req.path}    RequestType: ${req.query.q}    Question: ${req.query.d}`);
         res.send(resumeService.getTextResponse(req.query.q));
     }
 };
