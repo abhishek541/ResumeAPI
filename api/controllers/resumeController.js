60631fecaa0b0695c3a482424896e2404f79535c
Abhishek Kumar <ak6210@nyu.edu>
2019-08-05 21:39:35 -0400

    Added service for answering puzzle

--- api/controllers/resumeController.js
+++ api/controllers/resumeController.js
@@ -4,6 +4,10 @@ const resumeService = require('../service/resumeService');
 module.exports = {
     getSample(req, res, next) {
         logger.log("info", `${req.method} ${req.path}    RequestType: ${req.query.q}    Question: ${req.query.d}`);
-        res.send(resumeService.getTextResponse(req.query.q));
+        if(req.query.q === 'Puzzle') {
+            res.send(resumeService.getQueryAnswer(req.query.d));
+        } else {
+            res.send(resumeService.getTextResponse(req.query.q));
+        }
     }
 };
--- api/service/resumeService.js
+++ api/service/resumeService.js
@@ -2,9 +2,79 @@ const resumeData = require('./data');
 
 module.exports = {
     getTextResponse(query) {
-        if(query == "Puzzle") {
-            return resumeData[query].join('\n');
-        }
         return resumeData[query];
+    },
+
+    getQueryAnswer(queryStr) {
+        let symbolStr = queryStr.substring(queryStr.indexOf(':') + 1);
+        let strArr = symbolStr.split(" ");
+        let symbolArr = [];
+    
+        for(let i=2; i<strArr.length; i++) {
+          let temp = strArr[i].split("");
+          symbolArr.push(temp.slice(1));
+        }
+    
+        let row = symbolArr.length;
+        let col = symbolArr[0].length;
+    
+        for(let i=0; i<row; i++){
+          for(let j=0; j<col; j++){
+            if(i === j){ 
+              symbolArr[i][j] = '=';
+            }else if(symbolArr[i][j] === '<'){ 
+              symbolArr[j][i] = '>';
+            }else if(symbolArr[i][j] === '>'){
+              symbolArr[j][i] = '<';
+            }
+          }
+        }
+    
+        for(let i=0; i<row; i++){
+          for(let j=0; j<col; j++){
+            if(symbolArr[i][j] === '-'){
+              if(isGreaterCount(symbolArr[i])){
+                symbolArr[i][j] = '>';
+                symbolArr[j][i] = '<';
+              } else if(!isGreaterCount(symbolArr[i])) {
+                symbolArr[i][j] = '<';
+                symbolArr[j][i] = '>';
+              }
+            }
+          }
+        }
+    
+      let answer = ` ABCD\n`;
+      for(let i=0; i<row; i++){
+          answer += strArr[1].charAt(i);
+          for(let j=0; j<col; j++){
+            answer += symbolArr[i][j];
+            if(j == 3) {
+              answer += `\n`;
+            }
+          }
+      }
+    
+      return answer;
     }
+}
+
+function isGreaterCount(symbolRow) {
+  let count1 = 0
+  let count2 = 0;
+
+  for (let i=0; i<symbolRow.length; i++){
+    if(symbolRow[i] === '<'){
+      count1++;
+    }
+    if(symbolRow[i] === '>'){
+      count2++;
+    }
+  }
+
+  if(count2 > count1){
+    return true;
+  } else if(count1 > count2){
+    return false;
+  }
 }
